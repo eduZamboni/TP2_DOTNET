295b3811c6d2cab59cae0b17d1f29d1763344a32
eduZamboni <128424472+eduZamboni@users.noreply.github.com>
2025-05-19 14:36:23 -0300

    EX5

---
+++ Pages/CountryManager/CreateCountry.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace TP2.Pages.CountryManager
+{
+    // Classe de domínio Country
+    public class Country
+    {
+        public string CountryName { get; set; } = string.Empty;
+        public string CountryCode { get; set; } = string.Empty;
+    }
+
+    public class CreateCountryModel : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public bool Submitted { get; private set; }
+        public Country CreatedCountry { get; private set; }
+
+        public void OnGet()
+        {
+            Input = new InputModel();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Criar instância de Country a partir do InputModel
+            CreatedCountry = new Country
+            {
+                CountryName = Input.CountryName,
+                CountryCode = Input.CountryCode
+            };
+            
+            Submitted = true;
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "O nome do país é obrigatório.")]
+            [MinLength(3, ErrorMessage = "O nome do país deve ter ao menos 3 caracteres.")]
+            [Display(Name = "Nome do País")]
+            public string CountryName { get; set; } = string.Empty;
+            
+            [Required(ErrorMessage = "O código do país é obrigatório.")]
+            [Display(Name = "Código do País")]
+            public string CountryCode { get; set; } = string.Empty;
+        }
+    }
+}
--- obj/Debug/net9.0/TP2.AssemblyInfo.cs
+++ obj/Debug/net9.0/TP2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82500c66fdd7527d247c58ffde64069a51ccc02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30656624b8bb098db39b0b24cd7202bf60ed74e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
